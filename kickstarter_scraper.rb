706286003d0cc83fd538fd701a947f9b34c54971
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-05-27 22:40:59 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,7 +1,11 @@
 # require libraries/modules here
 require 'nakogiri'
+require 'pry'
 def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nakogiri::HTML(html)
+  binding.pry
 end
+
+Kick.create_project_hash
